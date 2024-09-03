06c964938aaac2b023ded6a9167d779e9bfb0b9d
JoannahP <joannahprabhakar701@gmail.com>
2024-09-03 10:49:52 +0530

    test message

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  envDir: './env',
+  envPrefix:'REACT_APP_',
+  server:{
+    port:3000,
+    strictPort: true,
+    open: true,
+  },
+  preview:{
+    port: 80,
+    strictPort:true,
+    open:true,
+  } ,
+  build: {
+    port: 80,
+    strictPort:true,
+  }
+});
